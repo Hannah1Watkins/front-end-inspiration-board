3dd78a846abe7329c040f071e143a162148f733b
maggie weir <mlweir98@gmail.com>
2023-06-27 11:14:47 -0400

    imported CardForm to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import BoardForm from "./components/BoardForm";
+import BoardForm from "./components/BoardForm.js";
+import CardForm from './components/CardForm.js';
 
 function App() {
   return (
-    <>
-    </>
+    <div></div>
   );
 }
 
