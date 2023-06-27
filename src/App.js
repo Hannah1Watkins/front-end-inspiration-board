d5fb70bba7b41328813d1296dd1f101bc93b6cf6
Hannah <hannahrwatkins@gmail.com>
2023-06-27 11:14:39 -0400

    set up app.js file to render BoardForm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import BoardForm from "./components/BoardForm";
 
 function App() {
   return (
-    <>
-    </>
+    <div className="App">
+      <header className="app-headder">
+        <BoardForm />
+        
+        </header>
+      </div>
   );
 }
 
