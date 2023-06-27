5e2f716c1943405de833338ee00f7766df28cc31
Elaine Watkins <ewatki.ga@gmail.com>
2023-06-27 13:52:14 -0400

    updated BoardForm to add Preview section

--- src/components/BoardForm.js
+++ src/components/BoardForm.js
@@ -1,27 +1,46 @@
 import React from 'react'
 
 const BoardForm = () => {
-  return (
-    <form className="new_board_form">
-        <section>
-            <h2>New Board: </h2>
-            <div className="new_board_fields">
-                <div>
-                    <label htmlFor="title">Title</label>
-                    <input name="title" value=""></input>
+    const [formFields, setFormFields] = React.useState({
+        title: '',
+        owner: ''
+    });
+
+    const handleChange = (e) => {
+        setFormFields({ ...formFields, [e.target.name] : e.target.value });
+    }
+    return (
+        <form className="new_board_form">
+            <section>
+                <h2>New Board: </h2>
+                <div className="new_board_fields">
+                    <div>
+                        <label htmlFor="title">Title: </label>
+                        <input 
+                        name="title" 
+                        value={formFields.title} 
+                        onChange={handleChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="owner">Owner: </label>
+                        <input 
+                        name="owner" 
+                        value={formFields.owner} 
+                        onChange={handleChange} />
+                    </div>
+                    <div>
+                        <h3>Preview: </h3>
+                        <section>
+                            Title: {formFields.title}
+                            Owner: {formFields.owner}
+                        </section>
+
+                    </div>
+                    <button className="button new_board_submit" type="submit" value="add_board">Submit</button>
                 </div>
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <input name="name"></input>
-                </div>
-                <div>
-                    {/* preview section */}
-                </div>
-                <button className="button new_board_submit" type="submit" value="add_board">Submit</button>
-            </div>
-        </section>
-    </form>
-  )
+            </section>
+        </form>
+    )
 }
 
 export default BoardForm;
