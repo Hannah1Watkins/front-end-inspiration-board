d1349c76df9af8505e80a6a1270843c2ee0b9dd6
maggie weir <mlweir98@gmail.com>
2023-07-19 18:02:03 -0400

    fixed some small bugs that happened during merge

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,24 +1,44 @@
 import React, {useState, useEffect} from 'react';
-import BoardForm from "./BoardForm";
-import NavBar from './NavBar';
-import SelectedBoard from './SelectedBoard';
-import LoginPage from './LoginPage.js'
-import NewUser from './NewUser.js'
+import Barbies from './Barbies.js';
+import BoardForm from './BoardForm.js';
+import CardForm from './CardForm.js';
+import CardsList from './CardsList.js';
+import NavBar from './NavBar.js';
+import SelectedBoard from './SelectedBoard.js';
+import '../App.css';
+
 
 const Dashboard = (props) => {
     return (
-    <div className="Dashboard">
+    <div className="App">
+
         <header className="app-header">
             <NavBar boards={props.boards} deleteBoard={props.deleteBoard} selectBoard={props.selectBoard}/>
         </header>
+
         {/* Get To Know The Team Section */}
         <section className="app-barbie-banner">
             <Barbies /> 
         </section>
-        <main>
-            {props.selectedBoard && <SelectedBoard selectedBoard={props.selectedBoard} cards={props.cards} createCard={props.createCard} deleteCard={props.deleteCard}increaseLikedCount={props.increaseLikedCount}/>}
-            <BoardForm createBoardCallback={props.createBoardCallback}></BoardForm>
+
+        <main className="app-body">
+            <aside>
+                <BoardForm createBoardCallback={props.createBoard}></BoardForm>
+                <CardForm createCard={props.createCard}/>
+            </aside>
+
+            <section>
+                {props.selectedBoard && <SelectedBoard selectedBoard={props.selectedBoard} cards={props.cards} createCard={props.createCard} deleteCard={props.deleteCard}increaseLikedCount={props.increaseLikedCount}/>}
+                <CardsList 
+                    cards={props.cards} 
+                    deleteCard = {props.deleteCard} 
+                    increaseLikedCount={props.increaseLikedCount}
+                    >
+                </CardsList>
+            </section>
         </main>
+        
+        
 
     </div>)
 }
