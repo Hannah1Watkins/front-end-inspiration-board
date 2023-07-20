a78cf81c2b4682b823735ff9fe6e865e0dc8bf2f
an2084 <angelanawang47@gmail.com>
2023-07-20 17:08:30 -0400

    Added OpeningPage code and css

--- src/components/OpeningPage.js
+++ src/components/OpeningPage.js
@@ -5,7 +5,7 @@ import './OpeningPage.css';
 function OpeningPage() {
   return (
     <div className='home-container'>
-        <video src="/videos/barbenheimer2.mp4" autoPlay/>
+        <video src="/videos/barbenheimer2.mp4" autoPlay loop muted/>
         <h1> WELCOME TO BARBENHEIMER WORLD</h1>
         <p>Choose your Character</p>
         <div className="home-btns">
